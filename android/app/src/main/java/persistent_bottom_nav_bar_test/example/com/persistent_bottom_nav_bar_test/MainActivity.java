68cf17fbd4d3f916ea1502e9fb84f3c9750fe0a4
Hussein <hussienalisy997test@gmile.com>
2023-07-23 08:28:58 +0300

    Initial commit

---
+++ android/app/src/main/java/persistent_bottom_nav_bar_test/example/com/persistent_bottom_nav_bar_test/MainActivity.java
@@ -0,0 +1,6 @@
+package persistent_bottom_nav_bar_test.example.com.persistent_bottom_nav_bar_test;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
